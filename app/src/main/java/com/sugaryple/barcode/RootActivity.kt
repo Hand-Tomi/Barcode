a65ed4c78022d9f782a9bc0f993d3c5cec324566
semyeong.son <seaud5@gmail.com>
2019-12-11 23:46:27 +0900

    権限取得コード整理

--- app/src/main/java/com/sugaryple/barcode/RootActivity.kt
+++ app/src/main/java/com/sugaryple/barcode/RootActivity.kt
@@ -1,10 +1,8 @@
 package com.sugaryple.barcode
 
 import android.Manifest
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import pub.devrel.easypermissions.AppSettingsDialog
 import pub.devrel.easypermissions.EasyPermissions
 import timber.log.Timber
 
@@ -17,7 +15,6 @@ class RootActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
 
     override fun onResume() {
         super.onResume()
-        Timber.v("onResume")
         if (hasPermissions().not()) {
             requestPermission()
         }
@@ -33,17 +30,17 @@ class RootActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
         Timber.v("requestPermission")
         EasyPermissions.requestPermissions(
             this,
-            "QRコードスキャンのためカメラ権限の許可が必要です",
-            0,
+            this.getString(R.string.permissions_dialog_rationale),
+            REQUEST_CODE_REQUEST_PERMISSION,
             *requiredPermissions
         )
     }
 
     override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
-        Timber.v("onPermissionsDenied")
         if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
-            Timber.v("AppSettingsDialog.Builder")
-            AppSettingsDialog.Builder(this).build().show()
+            // 必要な権限が永久に拒否された
+        } else {
+            // 必要な権限が拒否された
         }
     }
 
@@ -51,21 +48,16 @@ class RootActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
         Timber.v("onPermissionsGranted")
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        Timber.v("onActivityResult")
-        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE) {
-            Timber.v("DEFAULT_SETTINGS_REQ_CODE")
-        }
-    }
-
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        Timber.v("onRequestPermissionsResult")
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    companion object {
+        private const val REQUEST_CODE_REQUEST_PERMISSION = 10
     }
 }
